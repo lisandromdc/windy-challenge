b7c9b798fcbdf66acbe174e5e27a62c1abde1ebb
lisandromdc <lisandromdc@gmail.com>
2025-01-29 20:11:04 -0300

    base working

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  build: {
+    cssMinify: true,
+    ssr: false,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./app"),
+    },
+  },
+});
